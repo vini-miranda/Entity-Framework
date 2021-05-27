7f743bdc36f05527af8af10f2defe75522808915
Vinicius Miranda <v-alves-miranda@live.com>
2021-05-27 15:23:08 -0300

    Iniciando Entity Framework - ChangeTracker

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoCRUD.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class ProdutoCRUD
+    {
+        public static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            Produto p2 = new Produto();
+            p2.Nome = "Harry Potter e o Prisioneiro de Azkbahn";
+            p2.Categoria = "Livros";
+            p2.Preco = 15.50;
+
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                contexto.Adicionar(p2);
+            }
+        }
+
+        public static void AtualizarProduto(int id)
+        {
+            using (var context = new ProdutoDAOEntity())
+            {
+                var ItemToRefresh = context.Produtos().SingleOrDefault(x => x.Id == id);
+                if (ItemToRefresh != null)
+                {
+                    ItemToRefresh.Preco = 10.75;
+                    ItemToRefresh.Nome = "Harry Potter - Editado2";
+                    context.Atualizar(ItemToRefresh);
+                }
+            }
+            RecuperarProdutos();
+        }
+
+        public static void ExcluirUnicoItem(int id)
+        {
+
+            using (var context = new ProdutoDAOEntity())
+            {
+                var ItemToRemove = context.Produtos().SingleOrDefault(x => x.Id == id);
+                if (ItemToRemove != null)
+                {
+                    context.Remover(ItemToRemove);
+                }
+            }
+        }
+
+        public static void ExcluirProdutos()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = contexto.Produtos();
+                foreach (var item in produtos)
+                {
+                    contexto.Remover(item);
+                }
+            }
+        }
+
+        public static void RecuperarProdutos()
+        {
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = contexto.Produtos();
+                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome + "\t\t" + item.Categoria + " " + item.Preco);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using(var context = new LojaContext())
+            {
+                var produtos = context.Produtos.ToList();
+                foreach(var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine("================");
+
+                foreach(var e in context.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+
+                var p1 = produtos.First();
+                p1.Nome = "Novo Livro";
+                Console.WriteLine("================");
+
+
+                foreach (var e in context.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
